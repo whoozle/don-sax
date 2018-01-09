9e1434cc8d045e82e6c0e06af5162ed41ec9bf33
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-01-09 23:01:14 +0000

    added state dispatcher

--- cpp/src/jsonwc.cpp
+++ cpp/src/jsonwc.cpp
@@ -2,10 +2,19 @@
 
 namespace jsonwc
 {
-	Parser::Parser(IObjectReceiver *root): _current(root)
-	{ }
+	Parser::Parser(IObjectReceiver *root): _currentState(&_objectState), _jsonObjectState(root)
+	{
+	}
 
-	void Parser::Feed(const void *data, size_t size)
-	{ }
+	void Parser::Feed(const char *data, size_t size)
+	{
+		size_t offset = 0;
+		while(offset < size)
+		{
+			auto result = _currentState->Feed(data + offset, size - offset);
+			_currentState = result.NewState;
+			offset += result.ReadData;
+		}
+	}
 
 }
--- cpp/src/jsonwc.h
+++ cpp/src/jsonwc.h
@@ -60,12 +60,49 @@ namespace jsonwc
 
 	class Parser
 	{
-		IReceiver * _current;
+		struct IState;
+
+		struct Result
+		{
+			IState *	NewState;
+			size_t		ReadData;
+		};
+
+		struct IState
+		{
+			virtual ~IState() { }
+			virtual Result Feed(const char *data, size_t size) { return Result { this, size }; }
+		};
+
+		template<typename ReceiverType>
+		struct State : public IState
+		{
+			ReceiverType * Receiver;
+
+			State(ReceiverType *recv = nullptr) : Receiver(recv) { }
+		};
+
+		IState * _currentState;
+
+		class StringState : public State<IJsonValueReceiver>
+		{ } _stringState;
+		class IntegerState : public State<IJsonValueReceiver>
+		{ } _integerState;
+		class NumberState : public State<IJsonValueReceiver>
+		{ } _numberState;
+		class JsonObjectState : public State<IJsonObjectReceiver>
+		{
+			using State::State;
+		} _jsonObjectState;
+		class ObjectState : public State<IObjectReceiver>
+		{ } _objectState;
+		class ArrayState : public State<IJsonArrayReceiver>
+		{ } _arrayState;
 
 	public:
 		Parser(IObjectReceiver *root);
 
-		void Feed(const void *data, size_t size);
+		void Feed(const char *data, size_t size);
 	};
 
 
