def72128687fb33e94d8f7dfbef722e40b9b56a9
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-01-12 16:41:40 +0000

    removed unused function

--- cpp/src/jsonwc.cpp
+++ cpp/src/jsonwc.cpp
@@ -5,44 +5,6 @@
 
 namespace jsonwc
 {
-	namespace
-	{
-		std::string ToString(int token)
-		{
-			switch(token)
-			{
-				case TNULL:
-					return "null";
-				case TFALSE:
-					return "false";
-				case TTRUE:
-					return "true";
-				case TINTEGER:
-					return "integer";
-				case TNUMBER:
-					return "number";
-				case TSTRING:
-					return "string";
-				case TIDENTIFIER:
-					return "identifier";
-				case TOBJECTBEGIN:
-					return "{";
-				case TOBJECTEND:
-					return "}";
-				case TARRAYBEGIN:
-					return "[";
-				case TARRAYEND:
-					return "]";
-				case TSEPARATOR:
-					return "separator(, or ;)";
-				case TCOLON:
-					return ":";
-				default:
-					return "unknown token";
-			}
-		}
-	}
-
 	Parser::Parser(IValueReceiver *root)
 	{ }
 
